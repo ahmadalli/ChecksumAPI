345fde1ac5c09f2f4004514e399d18a32b54ee11
ahmadali shafiee <mail@ahmadalli.net>
2017-11-01 13:01:48 +0330

    calculate check if force of file's checksum did not found in database

--- ChecksumAPI/Controllers/MD5Controller.cs
+++ ChecksumAPI/Controllers/MD5Controller.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ChecksumAPI.Controllers
 {
@@ -13,28 +15,57 @@ namespace ChecksumAPI.Controllers
     [Route("/MD5")]
     public class MD5Controller : Controller
     {
-        public MD5Controller(IConfiguration configuration)
+        private readonly IConfiguration _configuration;
+        private readonly CADbContext _context;
+        private readonly DbSet<FileChecksum> _set;
+
+        public MD5Controller(IConfiguration configuration, CADbContext context)
         {
-            Configuration = configuration;
+            _configuration = configuration;
+            _context = context;
+            _set = _context.Set<FileChecksum>();
         }
 
-        private IConfiguration Configuration { get; }
-
         [HttpGet]
-        public string MD5(string fileUrl, int? offsetPercent)
+        public IActionResult MD5(string fileUrl, byte? offsetPercent, bool force = false)
         {
-            byte[] result;
-            using (var webClient = new System.Net.WebClient())
+            if (offsetPercent > 49)
             {
-                result = webClient.DownloadData(fileUrl);
+                return BadRequest("offset must be less than 50 percent");
             }
-            var offset = result.Length * (offsetPercent ?? Convert.ToInt32(Configuration["MD5FileOffsetPercent"])) / 200;
-            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(result, offset, result.Length - offset);
 
-            return BitConverter
-                    .ToString(hash)
-                    .Replace("-", string.Empty)
-                    .ToLower();
+            Expression<Func<FileChecksum, bool>> predicate = fc => fc.FileUrl == fileUrl && fc.Algorithm == "MD5" && fc.OffsetPercent == offsetPercent;
+
+            if (force || !_set.Any(predicate))
+            {
+                byte[] result;
+                using (var webClient = new System.Net.WebClient())
+                {
+                    result = webClient.DownloadData(fileUrl);
+                }
+
+                for (byte op = 0; op < 50; op++)
+                {
+                    var offset = result.Length * (offsetPercent ?? Convert.ToInt32(_configuration["MD5FileOffsetPercent"])) / 200;
+                    byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(result, offset, result.Length - offset);
+                    var checksum = BitConverter
+                        .ToString(hash)
+                        .Replace("-", string.Empty)
+                        .ToLower();
+
+                    _set.Add(new FileChecksum
+                    {
+                        FileUrl = fileUrl,
+                        OffsetPercent = op,
+                        Algorithm = "MD5",
+                        Checksum = checksum
+                    });
+                }
+            }
+
+            _context.SaveChanges();
+
+            return Ok(_set.First(predicate).Checksum);
         }
     }
 }
