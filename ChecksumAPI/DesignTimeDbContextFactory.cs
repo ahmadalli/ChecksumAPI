b043eb98b517fdc2d2b809842ed86131199a804d
ahmadali shafiee <mail@ahmadalli.net>
2017-11-01 12:40:15 +0330

    ef tool config added

---
+++ ChecksumAPI/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ChecksumAPI
+{
+    class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CADbContext>
+    {
+        public CADbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CADbContext>();
+            optionsBuilder.UseSqlite("Data Source=checksum.db", sqlOptions => sqlOptions.MigrationsAssembly(typeof(CADbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new CADbContext(optionsBuilder.Options);
+        }
+    }
+}
