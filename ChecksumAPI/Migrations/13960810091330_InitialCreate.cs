6ddfc3bbda30500714934fda37ed9edce33a5a06
ahmadali shafiee <mail@ahmadalli.net>
2017-11-01 12:43:58 +0330

    InitialCreate migration

---
+++ ChecksumAPI/Migrations/13960810091330_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ChecksumAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileChecksums",
+                columns: table => new
+                {
+                    FileUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    OffsetPercent = table.Column<byte>(type: "INTEGER", nullable: false),
+                    Algorithm = table.Column<string>(type: "TEXT", nullable: false),
+                    Checksum = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileChecksums", x => new { x.FileUrl, x.OffsetPercent });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileChecksums");
+        }
+    }
+}
