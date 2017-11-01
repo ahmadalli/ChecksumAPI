edd186e36e05100f40626321b61889ea72948d26
ahmadali shafiee <mail@ahmadalli.net>
2017-11-01 12:29:59 +0330

    setting up dbcontext and model

---
+++ ChecksumAPI/CADbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ChecksumAPI
+{
+    public class CADbContext : DbContext
+    {
+        public CADbContext(DbContextOptions<CADbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=checksum.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Type[] types = typeof(CADbContext).GetTypeInfo().Assembly.GetTypes();
+            IEnumerable<Type> typesToRegister = types
+                .Where(type => type.GetInterfaces()
+                .Where(i => i.IsGenericType)
+                .Any(i => i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)));
+
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                ModelBuilderExtensions.AddConfiguration(modelBuilder, configurationInstance);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        public static void AddConfiguration<TEntity>(ModelBuilder modelBuilder, IEntityTypeConfiguration<TEntity> configuration)
+            where TEntity : class
+        {
+            configuration.Configure(modelBuilder.Entity<TEntity>());
+        }
+    }
+}
---
+++ ChecksumAPI/FileChecksum.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChecksumAPI
+{
+    public class FileChecksum
+    {
+        public string FileUrl { get; set; }
+        public byte OffsetPercent { get; set; }
+        public string Algorithm { get; set; }
+        public string Checksum { get; set; }
+
+        #region configurations
+        public class Configuration : IEntityTypeConfiguration<FileChecksum>
+        {
+            public void Configure(EntityTypeBuilder<FileChecksum> builder)
+            {
+                builder.ToTable(nameof(FileChecksum).Pluralize());
+                builder.HasKey(fc => new { fc.FileUrl, fc.OffsetPercent });
+            }
+        }
+        #endregion
+    }
+}
